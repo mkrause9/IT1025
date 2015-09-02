e9f285fad8ce304d0d19d7c1ca6d04df6d040723
mkrause9 <Malloryakrause@gmail.com>
2015-09-01 20:42:55 -0400

    Fuel

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿
+namespace example6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+           const double pricePerGallon = 2.35;
+
+
+           System.Console.WriteLine("Does the vehicle use deisel?");
+           bool isDeisel = bool.Parse(System.Console.ReadLine());
+
+            //Vehicle Make
+            //Vehicle Model
+            //Gallon Capacity
+            //Current Gallons
+            //MPG
+            //Deisel (true or false)
+
+           
+
+            //Range (MPG*Number Gallons)
+            //How much it will cost to fill up tank
+            //How much it will cost to completely fill up tank
+
+           System.Console.WriteLine("Enter Vehicle Year:");
+           string vehicleYear = System.Console.ReadLine();
+
+
+        }
+    }
+}
